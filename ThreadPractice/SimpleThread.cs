ad223bcef49d7f28a6f571e2a12515a4f181926c
jmrq-43 <rojasquinteroj26@gmail.com>
2025-02-10 22:51:14 -0500

    practices simple thread

---
+++ ThreadPractice/SimpleThread.cs
@@ -0,0 +1,30 @@
+﻿namespace ThreadPractice;
+
+public class SimpleThread
+{
+    public static void SayHelloThread()
+    {
+        Thread thread1 =  new Thread(SayHello1);
+        Thread thread2 = new Thread(SayHello2);
+        thread1.Start();
+        thread2.Start();
+    }
+
+    private static void  SayHello1()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("hello Froma SayHello1");
+            Thread.Sleep(100);
+        }
+    }
+
+    private static void SayHello2()
+    {
+        for (int i = 0; i < 100;  i++)
+        {
+            Console.WriteLine("hello Froma SayHello2");
+            Thread.Sleep(100);
+        }
+    }
+}
